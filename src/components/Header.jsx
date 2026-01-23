6093df7b44d9249c61d021ec1b26bcff20216dc2
EunHyeok <secun77@naver.com>
2026-01-23 15:48:31 +0900

    헤더 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,17 @@ import "./Header.css";
 const Header = () => {
   const location = useLocation();
   const tabs = [
-    { id: "stray", label: "유기동물 정보", path: "/stray/list" },
+    {
+      id: "stray",
+      label: "유기동물 정보",
+      path: "/stray/list?region=전체&category=개&page=0",
+    },
     { id: "festival", label: "반려동물 행사정보", path: "/festival/list" },
     { id: "facility", label: "반려동물 관련 시설 정보", path: "/map" },
   ];
 
   const isActive = (path) => {
-    if (path === "/stray/list") {
+    if (path === "/stray/list?region=전체&category=개&page=0") {
       return location.pathname.startsWith("/stray");
     }
     if (path === "/festival/list") {
