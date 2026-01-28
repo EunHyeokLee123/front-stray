6158711e57a70cef1c70e9c4d0656a1f31e1d559
EunHyeok <secun77@naver.com>
2026-01-28 14:55:34 +0900

    sitemap 생성기 삭제

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import Sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // ✅ sitemap 자동 생성
-    Sitemap({
-      hostname: "https://nyangmong.com",
-    }),
   ],
 });
